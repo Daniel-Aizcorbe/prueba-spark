cb027de812baf1f48d15fc2c15b3e0a8a93bc403
Daniel-Aizcorbe <daizcorbe@frba.utn.edu.ar>
2022-11-04 03:02:51 -0300

    add HomeController

--- src/main/java/controllers/HomeController.java
+++ src/main/java/controllers/HomeController.java
@@ -9,4 +9,8 @@ public class HomeController {
   public ModelAndView getHome(Request request, Response response) {
     return new ModelAndView("233","home.hbs");
   }
+
+  public ModelAndView getHomeLogged(Request request, Response response) {
+    return new ModelAndView("233","home-logged.hbs");
+  }
 }
